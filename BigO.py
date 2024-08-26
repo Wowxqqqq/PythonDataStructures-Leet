0f3047de796115e326d4ee580c8daecbca9e40cc
Wowxqqqq <wowxqqq@gmail.com>
2024-08-26 20:21:21 +0200

    Done with Drop non-dominants.

--- BigO.py
+++ BigO.py
@@ -9,9 +9,20 @@
 
 # O(n**2)
 # Less efficient from a time complexity standpoint.
+# def print_items(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print(i, j)
+#
+# print_items(10)
+
+# Drop Non-Dominants
 def print_items(n):
     for i in range(n):
         for j in range(n):
             print(i, j)
+
+    for k in range(n):
+        print(k)
 
 print_items(10)
