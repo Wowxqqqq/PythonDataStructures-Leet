73ec662eb1d3f2a0d72da990e2b4f1e3ac9dc10c
Wowxqqqq <wowxqqq@gmail.com>
2024-08-26 20:16:04 +0200

    Getting work done with some O(n**2).

--- BigO.py
+++ BigO.py
@@ -1,6 +1,17 @@
 # O(n)
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+#     for j in range(n):
+#         print(j)
+#
+# print_items(10)
+
+# O(n**2)
+# Less efficient from a time complexity standpoint.
 def print_items(n):
     for i in range(n):
-        print(i)
+        for j in range(n):
+            print(i, j)
 
 print_items(10)
